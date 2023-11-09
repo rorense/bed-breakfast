64bc009190a5d76ad34cab9c2a3e93f49331f881
rorense <rkorense@gmail.com>
2023-11-10 09:24:36 +1300

    Working on register modal

--- bnb/app/components/modals/RegisterModal.tsx
+++ bnb/app/components/modals/RegisterModal.tsx
@@ -4,10 +4,56 @@ import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
-import { FieldValue, SubmitHandler, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, set, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import { on } from "events";
 
+// Component for entering register details
 const RegisterModal = () => {
-  return <div></div>;
+  const RegisterModal = useRegisterModal();
+  const [loading, setLoading] = useState(false);
+
+  // Initialising react-hook-form variables
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // Action when submit the login form
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    axios
+      .post("api/register", data)
+      .then(() => {
+        RegisterModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={loading}
+      isOpen={RegisterModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={RegisterModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
 };
 
 export default RegisterModal;
--- bnb/app/components/navbar/UserMenu.tsx
+++ bnb/app/components/navbar/UserMenu.tsx
@@ -3,9 +3,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const registerModal = useRegisterModal();
 
   //   function for toggling menu onClick
   const toggleOpen = useCallback(() => {
@@ -36,7 +38,7 @@ const UserMenu = () => {
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <>
-              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={registerModal.onOpen} label="Login" />
               <MenuItem onClick={() => {}} label="Signout" />
             </>
           </div>
