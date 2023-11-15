b727dc230aa4a0a3887d900b5801534085bbdba3
rorense <rkorense@gmail.com>
2023-11-16 09:12:47 +1300

    Created a new type safe User type

--- bnb/app/components/navbar/UserMenu.tsx
+++ bnb/app/components/navbar/UserMenu.tsx
@@ -5,11 +5,11 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface UserMenuPops {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 // User menu component for login and signout
