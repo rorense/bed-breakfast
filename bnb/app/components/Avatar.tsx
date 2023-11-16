ec8b5afcc2f6e574ac725a82246ddde57511e530
rorense <rkorense@gmail.com>
2023-11-16 15:53:06 +1300

    FInished authetication

--- bnb/app/components/Avatar.tsx
+++ bnb/app/components/Avatar.tsx
@@ -2,14 +2,18 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface Avatarprops {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<Avatarprops> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       height="30"
       width={30}
       alt="Avatar"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
     />
   );
 };
--- bnb/app/components/navbar/UserMenu.tsx
+++ bnb/app/components/navbar/UserMenu.tsx
@@ -54,7 +54,7 @@ const UserMenu: React.FC<UserMenuPops> = ({ currentUser }) => {
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
