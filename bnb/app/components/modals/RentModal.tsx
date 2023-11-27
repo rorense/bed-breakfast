97ce17d9f3a190a414826a6ecaefbd1571fed416
rorense <rkorense@gmail.com>
2023-11-28 10:54:53 +1300

    Working on category input

--- bnb/app/components/modals/RentModal.tsx
+++ bnb/app/components/modals/RentModal.tsx
@@ -2,6 +2,10 @@
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
   CATEGORY = 0,
@@ -16,6 +20,40 @@ const RentModal = () => {
   const rentModal = useRentModal();
   // Bnb details
   const [step, setStep] = useState(STEPS.CATEGORY);
+
+  // Initialising form for category input
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  // To watch for changes in category input when clicked
+  const category = watch("category");
+
+  // Custom hook for setting custom value
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
 
   //   Back a step
   const onBack = () => {
@@ -43,6 +81,31 @@ const RentModal = () => {
     return "Back";
   }, [step]);
 
+  // Changing body content based on step
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describe you?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {/* Mapping through each category */}
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => {
+                setCustomValue("category", category);
+              }}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       title="Bnb your home"
@@ -52,6 +115,7 @@ const RentModal = () => {
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      body={bodyContent}
     />
   );
 };
